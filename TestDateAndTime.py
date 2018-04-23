9353832db44f06e5e9ba564cf2b2101b9cb8da9b
xuxiaoming <1229717912@qq.com>
2018-04-23 13:17:48 +0800

    python字典、日期和时间

---
+++ TestDateAndTime.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- encoding；UTF-8 -*-
+
+import time
+
+ticks = time.time()
+print("当前时间戳：",ticks)
---
+++ TestDictionary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- encoding；UTF-8 -*-
+
+
+ 
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'};
+ 
+del dict['Name']; # 删除键是'Name'的条目
+
+print(dict)
+
+dict.clear();     # 清空词典所有条目
+
+print(dict)
+
+del dict ;        # 删除词典
+
+print(dict)
