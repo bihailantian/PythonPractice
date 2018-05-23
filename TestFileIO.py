92ab4f1fd0d1df3d51715c02850c5843c1053263
xuxiaoming <1229717912@qq.com>
2018-05-23 17:39:25 +0800

    文件I/O

---
+++ TestFileIO.py
@@ -0,0 +1,15 @@
+# -*- coding:UTF-8 -*-
+# 
+# Python 文件I/O
+
+print("Python 文件I/O")
+
+
+print("\n=================键盘输入======================\n")
+
+#str = input("请输入:")
+#print("输入的内容是:",str)
+
+
+str2 = raw_input("2请输入:")
+print("2输入的内容是:",str2)
