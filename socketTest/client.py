d68c43211cd85ec4b3e7c5b459525c500168ecfa
xuxiaoming <1229717912@qq.com>
2018-06-07 15:54:13 +0800

    python socket 完成

--- socketTest/client.py
+++ socketTest/client.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python3
-# 文件名：client.py
+# !/use/bin/python3
+#  -*- coding:UTF-8 -*-
 
-# 导入 socket、sys 模块
+# 1、导入socket、sys模块
 import socket
 import sys
 
-# 创建 socket 对象
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+# 2、创建socket对象
+clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# 获取本地主机名
-host = socket.gethostname() 
+# 3、获取本地主机名
+host = socket.gethostname()
 
-# 设置端口号
+# 4、绑定端口号,连接服务端
 port = 9999
+clientSocket.connect((host,port))
 
-# 连接服务，指定主机和端口
-s.connect((host, port))
+# 5、接收小于1024字节的数据
+msg = clientSocket.recv(1024)
 
-# 接收小于 1024 字节的数据
-msg = s.recv(1024)
+clientSocket.close()
 
-s.close()
+print(msg.decode('utf-8'))
 
-print (msg.decode('utf-8'))
+
--- socketTest/server.py
+++ socketTest/server.py
@@ -1,29 +1,31 @@
 # !/use/bin/python3
-# -*- coding:UTF-8 -*-
+#  -*- coding:UTF-8 -*-
 
+# 1、导入socket、sys模块
 import socket
 import sys
 
-
+# 2、创建socket对象
 serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# 获取本地主机名
+# 3、获取本地主机名
 host = socket.gethostname()
 
+# 4、绑定端口号
 port = 9999
-
-# 绑定端口号
 serverSocket.bind((host,port))
 
+# 5、设置最大连接数，超过后排队
 serverSocket.listen(5)
 
+# 6、建立客户端连接
 while True:
-	# 建立客户端连接
-	clientsocket,addr = serverSocket.accept()
+	clientSocket,addre = serverSocket.accept()
+	print(addre)
+	msg = "客户端，您好"
+	clientSocket.send(msg.encode('utf-8'))
+	clientSocket.close()
 
-	print("连接地址: %s" % str(addr))
-	
-	msg='欢迎访问菜鸟教程！'+ "\r\n"
-	clientsocket.send(msg.encode('utf-8'))
-	clientsocket.close()
+
+
 
