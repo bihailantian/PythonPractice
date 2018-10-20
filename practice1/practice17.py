9a519241ff2ffc28fcdf16b55875c19f002790b0
xuxiaoming <1229717912@qq.com>
2018-10-20 11:09:41 +0800

    practice17

--- practice1/practice17.py
+++ practice1/practice17.py
@@ -5,3 +5,20 @@
 题目：求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。例如2+22+222+2222+22222(此时共有5个数相加)，几个数相加由键盘控制。
 """
 
+a = input("请输入s=a+aa+aaa+aaaa+aa...a的a的值：\n")
+
+numStr = input("请输入相加的个数：")
+s = 0
+str = ""
+if numStr.isalnum():
+	num = int(numStr)
+	for i in range(num):
+		print("a=",a)
+		print("i=",i)
+		addition = a * (i+1)
+		print("addition=",addition)
+		s = s + int(addition)
+		str = str + "+" + addition
+	print(str[1:],"=",s)
+else:
+	print("请输入数字！")
