2009dc79ad52ab653889c3cee7e81e18b6378b92
xuxiaoming <1229717912@qq.com>
2018-10-21 18:39:14 +0800

    practice28,29

--- practice1/practice28.py
+++ practice1/practice28.py
@@ -5,3 +5,11 @@
 题目：给一个不多于5位的正整数，要求：一、求它是几位数，二、逆序打印出各位数字。
 """
 
+num = int(input("请不多于5位的正整数：\n"))
+
+numStr = str(num)
+
+length = len(numStr)
+
+if length <= 5:
+    print("%d位数，%s" % (length,numStr[::-1]))
