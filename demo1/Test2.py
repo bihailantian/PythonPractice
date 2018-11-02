f4fa73a0c80851e6ca2685fd563a4b851016d636
xuxiaoming <1229717912@qq.com>
2018-11-02 17:32:13 +0800

    激活码保存的数据库

--- demo1/Test2.py
+++ demo1/Test2.py
@@ -6,17 +6,24 @@ import pymysql
 
 def connectDB(sql):
     # 打开数据库连接      ip         用户名  密码   数据名称
-    db = pymysql.connect("localhost","root","root","py_test" ) 
+    conn = pymysql.connect("localhost","root","root","py_test" ) 
      
     # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
+    cursor = conn.cursor()
      
     # 使用 execute()  方法执行 SQL 查询 
-    cursor.execute(sql)
+    rows = cursor.execute(sql)
+    print("影响行数：%d" % rows)
+    #需要注意的是:在执行完插入或删除或修改操作后,需要调用一下conn.commit()方法进行提交.这样,数据才会真正保存在数据库中。
+    #否则数据不会保留在数据库中，rows也不会为0
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 if __name__ == '__main__':
     activity_code = genCode.genCode(200)
     #print("','".join(activity_code))
-    sqlValues = "','".join(activity_code)
-    sql = "insert into activity_code (activity_code) values (" + sqlValues + ")"
+    sqlValues = "'),('".join(activity_code)
+    sql = "insert into activity_code (activity_code) values ('" + sqlValues + "')"
+    #print(sql)
     connectDB(sql)
